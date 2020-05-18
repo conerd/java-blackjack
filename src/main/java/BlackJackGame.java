4c1434312278c6e83e719535c2f64ef9e5362c5a
KNY <kny011598@gmail.com>
2020-05-18 18:24:11 +0900

    feat: player 모두 2장씩 카드 지급

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -1,13 +1,44 @@
+import domain.card.Card;
+import domain.card.CardFactory;
+import domain.user.Player;
 import domain.user.Players;
 import view.InputView;
 
+import java.util.*;
+
 public class BlackJackGame {
+    private static final int FIRST_DISTRIBUTION_COUNT = 2;
+    private static Queue<Card> cards = new LinkedList<>();
+    private static List<Player> players = new ArrayList<>();
 
     public BlackJackGame() {
+        initCards();
+    }
 
+    private void initCards() {
+        List<Card> initialCards = CardFactory.create();
+        Collections.shuffle(initialCards);
+
+        for (Card card : initialCards) {
+            cards.offer(card);
+        }
     }
 
     public void run() {
-        Players players = new Players(InputView.inputPlayerNames());
+        players = Players.createPlayers(InputView.inputPlayerNames());
+        splitCards();
+
+    }
+
+    private void splitCards() {
+        for (Player player : players) {
+            splitCardsTwice(player);
+        }
+    }
+
+    private void splitCardsTwice(Player player) {
+        for (int i = 0; i < FIRST_DISTRIBUTION_COUNT; i++) {
+            player.addCard(cards.poll());
+        }
     }
 }
