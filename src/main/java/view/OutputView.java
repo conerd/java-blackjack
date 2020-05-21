d5ade657e297e2d1c7a561b02f4056c1770bdce6
KNY <kny011598@gmail.com>
2020-05-21 23:27:44 +0900

    style: 출력 문자열 static 변수 설정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,10 +3,13 @@ package view;
 import java.util.List;
 
 public class OutputView {
-    public static void printProgressGuide(String dealerName, List<String> playerNames) {
-        String players = String.join(", ", playerNames);
+    private static final String NEW_LINE = "\n";
+    private static final String NAME_JOINER = ", ";
 
-        System.out.println("\n" + dealerName + "와 " + players + "에게 2장의 카드를 나누었습니다.");
+    public static void printProgressGuide(String dealerName, List<String> playerNames) {
+        String players = String.join(NAME_JOINER, playerNames);
+
+        System.out.println(NEW_LINE + dealerName + "와 " + players + "에게 2장의 카드를 나누었습니다.");
     }
 
     public static void printPlayerCards(String name, String playerCards) {
